738ce9fb9ec54810a6cf946c857eb97be552893e
Mark Hallett <markhallett5@outlook.com>
2025-08-12 22:02:10 +0100

    added bag context and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth - do not delete
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',  # custom context processor for bag
             ],
         },
     },
@@ -160,3 +161,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
