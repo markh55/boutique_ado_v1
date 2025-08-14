295da5988bf1bb5f3812d1a101a9f0519d8e3e70
Mark Hallett <markhallett5@outlook.com>
2025-08-14 20:16:44 +0100

    added quantity boxes to shopping bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,4 @@
+
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -29,7 +30,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
