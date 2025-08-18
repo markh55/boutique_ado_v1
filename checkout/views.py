ce27c7ab3d1728808a23c41743b93d61ad9f7123
Mark Hallett <markhallett5@outlook.com>
2025-08-18 22:09:00 +0100

    Added stripe payment system

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RxZrIJDNpQfwITIBNzdHHtSpMiKd1j704EBaWx8dTlLQLY9DfaVWUJYLEZFegHT1s5ovLo7wcUXAv0TlrjDmIZL00Ekdcxt6M',
+        'client_secret': 'test_client_secret',  # Replace with actual client secret from Stripe
     }
 
     return render(request, template, context)
