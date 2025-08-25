cc6a96f1fc8fa58df094aa7f146ed2165653da11
Mark Hallett <markhallett5@outlook.com>
2025-08-25 23:27:52 +0100

    test

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -159,7 +159,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # AWS S3 Settings for static and media files
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutque-ado-moh' # change this to your AWS bucket name
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-moh'  # fixed typo
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
